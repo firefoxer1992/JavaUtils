1d7beb6607fdec821128554d89b5628421de2fd2
fenglin <huaiyinnianshao28@163.com>
2018-03-02 11:40:52 +0800

    增删改查xml节点和属性

--- src/main/java/com/XmlUtils.java
+++ src/main/java/com/XmlUtils.java
@@ -6,41 +6,13 @@ import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
 
 import java.io.*;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by lenovo on  二月
  */
 public class XmlUtils {
-    /**
-     * 获取xml文件的内容
-     * @param fileName 文件名，类似  String fileName1="E:\\test.xml";
-     * @param names   标签组成的字符数组.比如 String[] names=new String[]{"西游记","作者"};
-     * @throws Exception
-     */
-    public void getXmlContent(String fileName,String[] names) throws  Exception {
-        SAXReader saxReader=new SAXReader();
-        //获取文件，转换成Document对象
-        Document document=saxReader.read( new File(fileName));
-        //获取xml内容
-        String docXmlText=document.asXML();
-        System.out.println("xml内容如下："+docXmlText);
-        System.out.println("-----------------------------");
-        //获取根元素
-        Element e=document.getRootElement();
-        //遍历xml文档
-        for(String name : names) {
-                if (  e!=null &&  e.element(name)!=null) {
-                    e=e.element(name);
-                    String elementStr=e.asXML();
-                    System.out.println(elementStr);
-                    System.out.println("-----------------------------");
-          }
-        }
-         String text=e.getText();
-         System.out.println("元素内容如下:\n"+text);
-    }
-
-
     /**
      * 字符串写入xml文件
      * @param text   文本内容，类似 String text="<csdn><java>Java班</java><net>Net班</net></csdn>";
@@ -71,6 +43,11 @@ public class XmlUtils {
           xmlWriter.close();
     }
 
+    /**
+     * 创建Document对象，并往对象中添加节点元素，转存为xml文件
+     * @param fileName
+     * @throws Exception
+     */
     public void createDocumentToXml(String fileName) throws  Exception{
         Document document=DocumentHelper.createDocument();
         Element root=document.addElement("csdn");
@@ -81,16 +58,98 @@ public class XmlUtils {
         writeToXml(document,fileName);
     }
 
-//         public void getElementText() throws  Exception{
-//             SAXReader saxReader=new SAXReader();
-//             //获取文件，转换成Document对象
-//             Document document=saxReader.read( new File("E:\\test.xml"));
-//             Element root=document.getRootElement();
-//             Attribute attribute=root.attribute("四大名著");
-//             String text=attribute.getText();
-//             System.out.println(text);
-//
-//
-//
-//         }
+    /**
+     * 添加属性和数据到xml文件中
+     * @param fileName
+     */
+    public void addAttributeToXml(String fileName) throws Exception {
+        // 创建saxReader对象
+        SAXReader reader = new SAXReader();
+        // 通过read方法读取一个文件 转换成Document对象
+        Document document = reader.read(new File(fileName));
+        //获取根节点元素对象
+        Element root = document.getRootElement();
+        Element element=root.element("红楼梦");
+        Attribute attribute=element.attribute("id");
+        element.remove(attribute);
+        //添加新的元素
+        element.addAttribute("name","作者");
+        Element newElement=element.addElement("朝代");
+        newElement.setText("清朝");
+        //获得节点对象
+        Element author=element.element("作者");
+        element.addCDATA("红楼梦，见证一个大家族的兴衰");
+        writeToXml(document,fileName);
+    }
+
+
+    /**
+     * 遍历xml文件的节点
+     * @param fileName
+     * @throws Exception
+     */
+         public void getElementText(String fileName) throws  Exception{
+             SAXReader saxReader=new SAXReader();
+             //获取文件，转换成Document对象
+             Document document=saxReader.read( new File(fileName));
+             Element root=document.getRootElement();
+             //遍历所有元素的节点
+             listNodes(root);
+         }
+
+    /**
+     * 遍历当前节点元素下方的所有(元素)的子节点
+     * @param node
+     */
+    public void listNodes(Element node) {
+        System.out.println("当前节点的名称："+node.getName());
+        List<Attribute> attributeList=node.attributes();
+        System.out.println("节点的属性：");
+        for(Attribute attribute : attributeList) {
+            System.out.println(attribute.getText()+"---"+attribute.getName()
+            +"----"+attribute.getValue());
+        }
+        if( !("" ).equals( node.getTextTrim())){
+            System.out.println("文本内容:"+node.getText());
+        }
+        System.out.println("-------------------------");
+        Iterator<Element> iterator=node.elementIterator();
+        while (iterator.hasNext()) {
+           Element element=iterator.next();
+           listNodes(element);
+        }
+    }
+
+    /**
+     * 获取xml文件的内容
+     * @param fileName 文件名，类似  String fileName1="E:\\test.xml";
+     * @param names   标签组成的字符数组.比如 String[] names=new String[]{"西游记","作者"};
+     * @throws Exception
+     */
+    public void getXmlContent(String fileName,String[] names) throws  Exception {
+        SAXReader saxReader=new SAXReader();
+        //获取文件，转换成Document对象
+        Document document=saxReader.read( new File(fileName));
+        //获取xml内容
+        String docXmlText=document.asXML();
+        System.out.println("xml内容如下："+docXmlText);
+        System.out.println("-----------------------------");
+        //获取根元素
+        Element e=document.getRootElement();
+        //遍历xml文档
+        for(String name : names) {
+            if (  e!=null &&  e.element(name)!=null) {
+                e=e.element(name);
+                String elementStr=e.asXML();
+                System.out.println(elementStr);
+                System.out.println("-----------------------------");
+            }
+        }
+        if(e!=null) {
+            String text=e.getText();
+            System.out.println("元素内容如下:\n"+text);
+        }
+    }
+
+
 }
