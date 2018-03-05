64c348bf12a03085ca64a2e03adb94cb184431fe
fenglin <huaiyinnianshao28@163.com>
2018-03-05 17:26:26 +0800

    JsonUtil转换json和对象

---
+++ src/main/java/com/JsonUtils.java
@@ -0,0 +1,46 @@
+package com;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import java.util.List;
+
+/**
+ * Created by lenovo on  三月
+ */
+public class JsonUtils {
+    /**
+     * 将对象转化为json文本
+     * @param object
+     * @param <T>
+     * @return
+     */
+    public static <T> String objectToJson(Object object){
+        //将对象转化为Json数组
+        JSONObject jsonObject= JSONObject.fromObject(object);
+        return  jsonObject.toString();
+    }
+
+    /**
+     * 将List对象序列化为JSON文本
+     * @param list
+     * @param <T>
+     * @return
+     */
+    public static <T> String listToJson(List<T> list) {
+        JSONArray jsonArray = JSONArray.fromObject(list);
+        return jsonArray.toString();
+    }
+
+
+    /**
+     *  将Json对象转换为相关类型的对象
+     * @param jsonObject
+     * @param beanClass
+     * @return
+     */
+    public static Object jsonToObject(JSONObject jsonObject,Class beanClass){
+           return  JSONObject.toBean(jsonObject,beanClass);
+    }
+
+}
---
+++ src/main/java/com/bean/Person.java
@@ -0,0 +1,40 @@
+package com.bean;
+
+/**
+ * Created by lenovo on  三月
+ */
+public class Person {
+    private  String name;
+    private  String sex;
+    private  int age;
+
+    public Person(String name, String sex, int age) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
